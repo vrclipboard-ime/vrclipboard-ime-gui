364205f9145852dce313299ccb274f3058e4082b
mii <mii@sfc.wide.ad.jp>
2025-12-14 17:10:51 +0900

    npm install

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/sh -x
 export DOCKER_BUILDKIT=1 
 export SKIP_SWIFT_BUILD=1
 
 cd src-tauri/rs-azookey-binding
 docker build --target export --output type=local,dest=./ .
+
+npm install
