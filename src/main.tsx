6d8cd16aae94bc109cb491c366d0745b19504408
mii <mii@sfc.wide.ad.jp>
2025-12-14 21:47:54 +0900

    update rs-azookey-binding, fix UI fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@fontsource/noto-sans-jp";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
